2ea80dd4a17fbc7fc53f963a8cad139251bcb79c
Moinul Islam <moinulilm10@gmail.com>
2024-06-23 18:42:40 +0600

    "Added AWS configuration file and updated package.json"

---
+++ src/config/aws.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const AWS = require("aws-sdk");
+
+dotenv.config();
+
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  endpoint: process.env.AWS_ENDPOINT,
+});
+
+const dynamodbClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: process.env.AWS_API_VERSION,
+});
+
+module.exports = { dynamodbClient };
