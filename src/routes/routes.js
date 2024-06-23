0698a630000ebbbdfd3d3fa6c1e7e046c36e5786
Moinul Islam <moinulilm10@gmail.com>
2024-06-23 20:17:04 +0600

    Added @middy/http-error-handler and @middy/http-router dependencies, updated package.json and package-lock.json, and added handlers to routes in routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,31 @@
 const middy = require("@middy/core");
+const {
+  getAllUsersHandler,
+  createUserHandler,
+  updateUserHandler,
+  deleteUserHandler,
+} = require("../controllers/userController");
 
 const routes = [
   {
     method: "GET",
     path: "/users",
+    handler: getAllUsersHandler,
   },
   {
     method: "POST",
     path: "/users",
+    handler: createUserHandler,
   },
   {
     method: "PUT",
     path: "/users/{id}",
+    handler: updateUserHandler,
   },
   {
     method: "DELETE",
     path: "/users/{id}",
+    handler: deleteUserHandler,
   },
 ];
 
